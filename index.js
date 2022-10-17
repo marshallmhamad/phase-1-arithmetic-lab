4f3abd10d4d4ce929d93d678769219c1032cf24d
marshall <mhamad.marshall@gmail.com>
2022-10-17 20:17:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 13
+let num4 = 9
+
+let mod = num3 % num4
+
+
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
